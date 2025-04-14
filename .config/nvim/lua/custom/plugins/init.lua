a2abfd88c08f4c1daa18871873258f37e350f1be
Max Fung <maxhfung@gmail.com>
2025-04-14 13:59:16 -0700

    add typescript-tools and nvim-jdtls

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,22 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+  },
+  {
+    'mfussenegger/nvim-jdtls',
+    ft = { 'java' },
+    config = function()
+      local jdtls = require 'jdtls'
+      local root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew' }
+
+      if root_dir then
+        jdtls.start_or_attach { root_dir = root_dir }
+      end
+    end,
+  },
+}
