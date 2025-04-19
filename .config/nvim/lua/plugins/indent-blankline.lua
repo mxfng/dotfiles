0856e46dc082c397b16319d6d0073b34a68fa675
max fung <maxhfung@gmail.com>
2025-04-19 10:04:27 -0700

    add mini-indentscope from lazyvim

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,10 +2,32 @@
 
 return {
   'lukas-reineke/indent-blankline.nvim',
+  opts = function()
+    return {
+      indent = {
+        char = '│',
+        tab_char = '│',
+      },
+      scope = { show_start = false, show_end = false },
+      exclude = {
+        filetypes = {
+          'Trouble',
+          'alpha',
+          'dashboard',
+          'help',
+          'lazy',
+          'mason',
+          'neo-tree',
+          'notify',
+          'snacks_dashboard',
+          'snacks_notif',
+          'snacks_terminal',
+          'snacks_win',
+          'toggleterm',
+          'trouble',
+        },
+      },
+    }
+  end,
   main = 'ibl',
-  event = 'BufReadPre',
-  opts = {
-    indent = { char = '│' },
-    scope = { enabled = true },
-  },
 }
