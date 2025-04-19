ae23e389f51560942335867397360570ecf3dda7
max fung <maxhfung@gmail.com>
2025-04-19 03:29:17 -0700

    context colors

--- .config/nvim/lua/plugins/nvim-treesitter-context.lua
+++ .config/nvim/lua/plugins/nvim-treesitter-context.lua
@@ -2,5 +2,24 @@
 
 return {
   'nvim-treesitter/nvim-treesitter-context',
-  opts = { mode = 'cursor', max_lines = 0 },
+  opts = {
+    enable = true,
+    max_lines = 0,
+    min_window_height = 0,
+    line_numbers = true,
+    multiline_threshold = 20,
+    trim_scope = 'outer',
+    mode = 'cursor',
+    separator = '─',
+    zindex = 20,
+  },
+  config = function(_, opts)
+    require('treesitter-context').setup(opts)
+    
+    -- Set transparent background for context window
+    vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = 'NONE' })
+    
+    local comment_color = require('tokyonight.colors').setup().comment
+    vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = 'NONE', fg = comment_color })
+  end
 }
