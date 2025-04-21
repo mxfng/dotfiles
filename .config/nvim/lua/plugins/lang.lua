c070c417760d41f4cc418de69554711621be2311
max fung <maxhfung@gmail.com>
2025-04-21 00:10:27 -0700

    organize plugins

---
+++ .config/nvim/lua/plugins/lang.lua
@@ -0,0 +1,9 @@
+-- TypeScript Tools configuration
+
+return {
+  { -- TypeScript Tools
+    'pmizio/typescript-tools.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    opts = {},
+  },
+}
