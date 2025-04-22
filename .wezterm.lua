971c08459bcec21638e4482f423ca6db37557be3
Max Fung <maxhfung@gmail.com>
2025-04-22 10:28:32 -0700

    fix terminal color issues

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,24 +10,6 @@ end
 config.term = "wezterm"
 
 config.force_reverse_video_cursor = true
--- config.colors = {
--- 	foreground = "#dcd7ba",
--- 	background = "#05081c",
---
--- 	cursor_bg = "#000000",
--- 	cursor_fg = "#c8c093",
--- 	cursor_border = "#c8c093",
---
--- 	selection_fg = "#c8c093",
--- 	selection_bg = "#2d4f67",
---
--- 	scrollbar_thumb = "#16161d",
--- 	split = "#16161d",
---
--- 	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
--- 	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
--- 	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
--- }
 config.colors = theme.colors()
 
 config.window_background_opacity = 0.9
