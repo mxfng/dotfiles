f4eeeeb36343ccf7ce53d67d2535ba9750231357
max fung <maxhfung@gmail.com>
2025-04-21 22:16:20 -0700

    update ui

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       require("lualine").setup({
         options = {
-          theme = "tokyonight",
+          theme = "rose-pine",
         },
       })
     end,
