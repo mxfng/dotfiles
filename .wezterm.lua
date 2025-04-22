e3da317e94b0edeb9b6bb33e20e73e271b2bb11a
max fung <maxhfung@gmail.com>
2025-04-21 22:29:15 -0700

    rose-pine wezterm theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
+local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").moon
 
 local is_darwin = function()
 	return wezterm.target_triple:find("darwin") ~= nil
@@ -9,26 +10,27 @@ end
 config.term = "wezterm"
 
 config.force_reverse_video_cursor = true
-config.colors = {
-	foreground = "#dcd7ba",
-	background = "#05081c",
+-- config.colors = {
+-- 	foreground = "#dcd7ba",
+-- 	background = "#05081c",
+--
+-- 	cursor_bg = "#000000",
+-- 	cursor_fg = "#c8c093",
+-- 	cursor_border = "#c8c093",
+--
+-- 	selection_fg = "#c8c093",
+-- 	selection_bg = "#2d4f67",
+--
+-- 	scrollbar_thumb = "#16161d",
+-- 	split = "#16161d",
+--
+-- 	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+-- 	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+-- 	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+-- }
+config.colors = theme.colors()
 
-	cursor_bg = "#000000",
-	cursor_fg = "#c8c093",
-	cursor_border = "#c8c093",
-
-	selection_fg = "#c8c093",
-	selection_bg = "#2d4f67",
-
-	scrollbar_thumb = "#16161d",
-	split = "#16161d",
-
-	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-}
-
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 config.macos_window_background_blur = 98
 
