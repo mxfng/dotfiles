ba5d5af680428096b80dcc3727a29f1bf38758bd
Max Fung <maxhfung@gmail.com>
2025-04-23 11:59:08 -0700

    rm useless format attempt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,4 @@ if vim.loader then
   vim.loader.enable()
 end
 
--- Show sources if multiple LSPs
-vim.diagnostic.config({
-  virtual_text = {
-    source = "if_many",
-  },
-  float = {
-    source = true,
-  },
-})
-
 require("config.lazy")
