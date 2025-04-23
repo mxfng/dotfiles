a43d1a7d2c1ae0056d5589a72ba28227d4e7ff91
Max Fung <maxhfung@gmail.com>
2025-04-23 11:56:33 -0700

    update lsp config to show sources

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,14 @@ if vim.loader then
   vim.loader.enable()
 end
 
+-- Show sources if multiple LSPs
+vim.diagnostic.config({
+  virtual_text = {
+    source = "if_many",
+  },
+  float = {
+    source = true,
+  },
+})
+
 require("config.lazy")
