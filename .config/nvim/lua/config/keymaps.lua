229eb18966402aa66e189fa153fcdaca3a8c8b59
Max Fung <maxhfung@gmail.com>
2025-04-25 18:25:30 -0700

    improve keymaps code

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,42 +1,51 @@
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
 
-keymap.set("n", "x", '"_x')
+-- Default options for all mappings
+local default_opts = { noremap = true, silent = true }
+
+-- Helper function to create keymap with description
+local function map(mode, lhs, rhs, desc)
+  keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+end
+
+-- Basic operations
+map("n", "x", '"_x', "Delete without yanking")
 
 -- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+map("n", "+", "<C-a>", "Increment number")
+map("n", "-", "<C-x>", "Decrement number")
 
 -- Select all
-keymap.set("n", "<S-C-a>", "gg<S-v>G")
+map("n", "gA", "ggVG", "Select all text")
+map("n", "gyA", "ggVGy", "Select all text and yank")
 
 -- Save file and quit
-keymap.set("n", "<Leader>w", ":update<Return>", opts)
-keymap.set("n", "<Leader>q", ":quit<Return>", opts)
-keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
+map("n", "<Leader>w", ":update<Return>", "Save file")
+map("n", "<Leader>q", ":quit<Return>", "Quit")
+map("n", "<Leader>Q", ":qa<Return>", "Quit all")
 
 -- File explorer with NvimTree
-keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
-keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
+map("n", "<Leader>f", ":NvimTreeFindFile<Return>", "Find in NvimTree")
+map("n", "<Leader>t", ":NvimTreeToggle<Return>", "Toggle NvimTree")
 
 -- Tabs
-keymap.set("n", "te", ":tabedit")
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
-keymap.set("n", "tw", ":tabclose<Return>", opts)
+map("n", "te", ":tabedit", "New tab")
+map("n", "<tab>", ":tabnext<Return>", "Next tab")
+map("n", "<s-tab>", ":tabprev<Return>", "Previous tab")
+map("n", "tw", ":tabclose<Return>", "Close tab")
 
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+map("n", "ss", ":split<Return>", "Horizontal split")
+map("n", "sv", ":vsplit<Return>", "Vertical split")
 
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
+map("n", "sh", "<C-w>h", "Go to left window")
+map("n", "sk", "<C-w>k", "Go to top window")
+map("n", "sj", "<C-w>j", "Go to bottom window")
+map("n", "sl", "<C-w>l", "Go to right window")
 
 -- Resize window
-keymap.set("n", "<C-S-h>", "<C-w><")
-keymap.set("n", "<C-S-l>", "<C-w>>")
-keymap.set("n", "<C-S-k>", "<C-w>+")
-keymap.set("n", "<C-S-j>", "<C-w>-")
+map("n", "<C-S-h>", "<C-w><", "Decrease width")
+map("n", "<C-S-l>", "<C-w>>", "Increase width")
+map("n", "<C-S-k>", "<C-w>+", "Increase height")
+map("n", "<C-S-j>", "<C-w>-", "Decrease height")
