01a41a04fbe1d211aa8cfe5b508397b61b1f4820
mxfng <maxhfung@gmail.com>
2025-05-05 10:33:03 -0700

    add git_bulk_delete_branches function

---
+++ .config/fish/functions/git_bulk_delete_branches.fish
@@ -0,0 +1,28 @@
+function git_bulk_delete_branches
+    # Get all branches except the current one
+    set branches (git branch | string replace -r '^\*?\s*' '')
+
+    # Use fish's built-in selection functionality
+    set selected_branches (printf "%s\n" $branches | fzf -m)
+
+    # Check if any branches were selected
+    if test -n "$selected_branches"
+        # Confirm deletion
+        echo "You're about to delete these branches:"
+        printf "%s\n" $selected_branches
+
+        read -l -P "Are you sure? (y/N) " confirm
+
+        if test "$confirm" = y -o "$confirm" = Y
+            for branch in $selected_branches
+                echo "Deleting $branch..."
+                git branch -D $branch
+            end
+            echo "Branch cleanup complete!"
+        else
+            echo "Operation cancelled."
+        end
+    else
+        echo "No branches selected."
+    end
+end
