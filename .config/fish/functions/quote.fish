ec18094879eaa62aa8d38a7ee2e5416379c69be4
mxfng <maxhfung@gmail.com>
2025-05-05 10:33:33 -0700

    move quote.yml to fish/data

--- .config/fish/functions/quote.fish
+++ .config/fish/functions/quote.fish
@@ -1,15 +1,14 @@
-function quote -d "Display a random quote from wisdom.yml"
-    set -l script_dir (dirname (status filename))
-    set -l quote_file "$script_dir/quote.yml"
+function quote -d "Display a random quote from quotes.yml"
+    set -l quotes_file "$HOME/.config/fish/data/quotes.yml"
 
-    set -l categories (yq '.categories | keys | .[]' $quote_file)
+    set -l categories (yq '.categories | keys | .[]' $quotes_file)
     set -l category $categories[(random 1 (count $categories))]
 
-    set -l quote_count (yq ".categories.$category | length" $quote_file)
+    set -l quote_count (yq ".categories.$category | length" $quotes_file)
     set -l quote_index (random 0 (math $quote_count - 1))
 
-    set -l quote (yq ".categories[\"$category\"][$quote_index].quote" $quote_file)
-    set -l author (yq ".categories[\"$category\"][$quote_index].author" $quote_file)
+    set -l quote (yq ".categories[\"$category\"][$quote_index].quote" $quotes_file)
+    set -l author (yq ".categories[\"$category\"][$quote_index].author" $quotes_file)
 
     echo -n "❝ "
     echo -n $quote
