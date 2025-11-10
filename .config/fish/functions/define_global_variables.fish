d97d12785fb9a3bf0699a8a54a5653e235f718d3
mxfng <maxhfung@gmail.com>
2025-11-10 10:38:27 -0800

    Add GNU coreutils to replace BSD

--- .config/fish/functions/define_global_variables.fish
+++ .config/fish/functions/define_global_variables.fish
@@ -13,6 +13,13 @@ function define_global_variables -d 'Defines all and exclusively globally export
     set -gx GOBIN $GOPATH/bin
 
     fish_add_path \
+        /opt/homebrew/opt/coreutils/libexec/gnubin \
+        /opt/homebrew/opt/findutils/libexec/gnubin \
+        /opt/homebrew/opt/gnu-sed/libexec/gnubin \
+        /opt/homebrew/opt/gnu-tar/libexec/gnubin \
+        /opt/homebrew/opt/grep/libexec/gnubin \
+        /opt/homebrew/opt/gawk/libexec/gnubin \
+        /opt/homebrew/opt/gnu-which/libexec/gnubin \
         $HOME/.local/bin \
         $HOME/Developer/scripts
 end
