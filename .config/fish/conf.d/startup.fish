7430e572a3cce1698428aced9e6bc1c7a549217c
mxfng <maxhfung@gmail.com>
2025-11-10 12:46:12 -0800

    Migrate to auto-generated docker fish completions

--- .config/fish/conf.d/startup.fish
+++ .config/fish/conf.d/startup.fish
@@ -3,12 +3,21 @@ if test -z "$SSH_AUTH_SOCK"; and test -z "$SSH_AGENT_PID"; and test -z "$SSH_CLI
     eval (ssh-agent -c) >/dev/null 2>&1
 end
 
+# Generate completions for dependencies
 # This isn't the technically correct place for this, but it ensures completions are available
-# whenever asdf is installed or updated without manual user intervention.
-and mkdir -p ~/.config/fish/completions
-and ln -sf "(brew --prefix asdf)"/completions/asdf.fish ~/.config/fish/completions
+# whenever dependencies are installed or updated without manual user intervention.
+mkdir -p ~/.config/fish/completions
+
+if command -v asdf >/dev/null 2>&1
+    ln -sf (brew --prefix asdf)/completions/asdf.fish ~/.config/fish/completions 2>/dev/null
+end
+
+if command -v docker >/dev/null 2>&1
+    docker completion fish >~/.config/fish/completions/docker.fish 2>/dev/null
+end
 
 # Start window management services
+# Passwordless sudo is configured via ./scripts/yabai_sa_setup.fish or running ./provision desktop
 restart_wm --sudo
 
 # Auto-attach to tmux session
