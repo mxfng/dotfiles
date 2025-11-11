ee418362d78d16498c4623cdb4254ea4dec4ea3a
mxfng <maxhfung@gmail.com>
2025-11-10 16:34:08 -0800

    Extract fish completions to scripts/fish_generate_completions.fish and run in provision

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,12 +1,14 @@
 # Order matters: asdf shims must come first, then GNU tools, then custom paths
 
-# ASDF shims - must be first to override system versions
+# ASDF configuration code
 if test -z $ASDF_DATA_DIR
     set _asdf_shims "$HOME/.asdf/shims"
 else
     set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
 
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
 if not contains $_asdf_shims $PATH
     set -gx --prepend PATH $_asdf_shims
 end
---
+++ scripts/fish_generate_completions.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+
+# Generate fish shell completions for installed tools
+# This script should be run during provisioning or when tools are updated
+
+echo 'Generating fish completions...'
+
+set -l completions_dir ~/.config/fish/completions
+
+# Create completions directory if it doesn't exist
+mkdir -p $completions_dir
+
+# asdf
+if command -v asdf >/dev/null 2>&1
+    asdf completion fish >$completions_dir/asdf.fish 2>/dev/null
+else
+    echo 'warning: asdf not found'
+end
+
+# docker
+if command -v docker >/dev/null 2>&1
+    docker completion fish >$completions_dir/docker.fish 2>/dev/null
+else
+    echo 'warning: docker not found'
+end
+
+echo 'Done!'
