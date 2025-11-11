fa96bb0a2e1b01dbe42c54a9883c1ee846ac16a4
mxfng <maxhfung@gmail.com>
2025-11-10 16:40:14 -0800

    Move window management to startup.fish and guard with pgrep

--- .config/fish/conf.d/startup.fish
+++ .config/fish/conf.d/startup.fish
@@ -9,3 +9,10 @@ if status is-interactive
     and test -z "$VIM"
     tmux new-session -A -s main
 end
+
+# Start window management services if not running
+# Passwordless sudo is configured via ./scripts/yabai_setup_sa.fish or running ./provision desktop
+if not pgrep -x yabai >/dev/null 2>&1; or not pgrep -x skhd >/dev/null 2>&1
+    fish -c "restart_wm --sudo" &
+    disown
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,3 @@
 # Additional configuration is loaded automatically from conf.d/
 
 umask 077
-
-#     # Start window management services
-#     # Passwordless sudo is configured via ./scripts/yabai_setup_sa.fish or running ./provision desktop
-#     fish -c "restart_wm --sudo" &
-#     disown
