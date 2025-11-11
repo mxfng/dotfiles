127d3635b66f7a9ca141854c77404a14a965d881
mxfng <maxhfung@gmail.com>
2025-11-11 14:41:17 -0800

    Format whitespace on random_quote

--- .config/fish/functions/random_quote.fish
+++ .config/fish/functions/random_quote.fish
@@ -1,12 +1,9 @@
 function random_quote -d "Display a random quote from quotes.yml"
     set -l quotes_file "$HOME/.config/fish/data/quotes.yml"
-
     set -l categories (yq '.categories | keys | .[]' $quotes_file)
     set -l category $categories[(random 1 (count $categories))]
-
     set -l quote_count (yq ".categories.$category | length" $quotes_file)
     set -l quote_index (random 0 (math $quote_count - 1))
-
     set -l quote (yq ".categories[\"$category\"][$quote_index].quote" $quotes_file)
     set -l author (yq ".categories[\"$category\"][$quote_index].author" $quotes_file)
 
