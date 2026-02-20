6713ae83e6bd5a049f214173097ff1960996dfac
mxfng <maxhfung@gmail.com>
2026-02-20 12:59:58 -0800

    chore: Sync state directories via XDG paths for lesshist, psql, node, and npm

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -1,6 +1,7 @@
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
 set -gx XDG_DESKTOP_DIR $HOME/Desktop
 set -gx XDG_DOWNLOAD_DIR $HOME/Downloads
 set -gx XDG_DOCUMENTS_DIR $HOME/Documents
@@ -28,3 +29,8 @@ set -gx GOBIN $GOPATH/bin
 set -gx BUN_INSTALL_CACHE_DIR $HOME/.cache/bun
 
 set -gx DOCKER_CONFIG $HOME/.config/docker
+
+set -gx LESSHISTFILE $XDG_STATE_HOME/less/history
+set -gx PSQL_HISTORY $XDG_STATE_HOME/psql/history
+set -gx NODE_REPL_HISTORY $XDG_STATE_HOME/node/history
+set -gx NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
