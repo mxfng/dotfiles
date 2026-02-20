36a41cada77467399dc4122cd2146c3defd20855
mxfng <maxhfung@gmail.com>
2026-02-20 14:47:21 -0800

    chore: add colima completions

--- scripts/fish_generate_completions.fish
+++ scripts/fish_generate_completions.fish
@@ -17,6 +17,12 @@ else
     echo 'warning: asdf not found'
 end
 
+if command -v colima >/dev/null 2>&1
+    colima completion fish >$completions_dir/colima.fish 2>/dev/null
+else
+    echo 'warning: colima not found'
+end
+
 # docker
 if command -v docker >/dev/null 2>&1
     docker completion fish >$completions_dir/docker.fish 2>/dev/null
@@ -24,18 +30,12 @@ else
     echo 'warning: docker not found'
 end
 
-# kubectl
-if command -v kubectl >/dev/null 2>&1
-    kubectl completion fish >$completions_dir/kubectl.fish 2>/dev/null
+# git-lfs
+if command -v git >/dev/null 2>&1
+    and git lfs version >/dev/null 2>&1
+    git lfs completion fish >$completions_dir/git-lfs.fish 2>/dev/null
 else
-    echo 'warning: kubectl not found'
-end
-
-# k9s
-if command -v k9s >/dev/null 2>&1
-    k9s completion fish >$completions_dir/k9s.fish 2>/dev/null
-else
-    echo 'warning: k9s not found'
+    echo 'warning: git-lfs not found'
 end
 
 # k3d
@@ -45,17 +45,23 @@ else
     echo 'warning: k3d not found'
 end
 
+# k9s
+if command -v k9s >/dev/null 2>&1
+    k9s completion fish >$completions_dir/k9s.fish 2>/dev/null
+else
+    echo 'warning: k9s not found'
+end
+
+# kubectl
+if command -v kubectl >/dev/null 2>&1
+    kubectl completion fish >$completions_dir/kubectl.fish 2>/dev/null
+else
+    echo 'warning: kubectl not found'
+end
+
 # yq
 if command -v yq >/dev/null 2>&1
     yq shell-completion fish >$completions_dir/yq.fish 2>/dev/null
 else
     echo 'warning: yq not found'
-end
-
-# git-lfs
-if command -v git >/dev/null 2>&1
-    and git lfs version >/dev/null 2>&1
-    git lfs completion fish >$completions_dir/git-lfs.fish 2>/dev/null
-else
-    echo 'warning: git-lfs not found'
 end
