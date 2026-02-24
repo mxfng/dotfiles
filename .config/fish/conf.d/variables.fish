0f9de26b915fda525f7178433edc62113ef5dbb9
mxfng <maxhfung@gmail.com>
2026-02-23 18:32:52 -0800

    chore: Add kube prod context to fish variables

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -30,6 +30,8 @@ set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --exclude ".git/"'
 set -gx GOPATH $HOME/Developer/go
 set -gx GOBIN $GOPATH/bin
 
+set -gx KUBECONFIG $HOME/.kube/config:$HOME/.kube/prod.yaml
+
 set -gx LESSHISTFILE $XDG_STATE_HOME/less/history
 
 set -gx NODE_REPL_HISTORY $XDG_STATE_HOME/node/history
