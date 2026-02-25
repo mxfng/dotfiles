182b0df7bf8ce47433f43478af310d9702799193
mxfng <maxhfung@gmail.com>
2026-02-25 13:52:22 -0800

    chore: Update yabai_setup_sa to handle version updates

--- scripts/yabai_setup_sa.fish
+++ scripts/yabai_setup_sa.fish
@@ -13,10 +13,12 @@ set username (whoami)
 set sudoers_file /etc/sudoers.d/yabai
 
 # Configure passwordless sudo for yabai --load-sa
-if not test -f $sudoers_file
+set yabai_hash (shasum -a 256 $yabai_path | cut -d " " -f 1)
+set expected_entry "$username ALL=(root) NOPASSWD: sha256:$yabai_hash $yabai_path --load-sa"
+
+if not test -f $sudoers_file; or not sudo grep -qF "$yabai_hash" $sudoers_file
     echo 'configuring passwordless sudo for yabai --load-sa'
-    set yabai_hash (shasum -a 256 $yabai_path | cut -d " " -f 1)
-    echo "$username ALL=(root) NOPASSWD: sha256:$yabai_hash $yabai_path --load-sa" | sudo tee $sudoers_file >/dev/null
+    echo $expected_entry | sudo tee $sudoers_file >/dev/null
 
     if not sudo visudo -c -f $sudoers_file &>/dev/null
         echo 'error: invalid sudoers file'
