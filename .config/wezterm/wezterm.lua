23910e89e039caacc6b35d2656e3dbb45b775f60
mxfng <m@maxfu.ng>
2026-03-17 00:48:37 -0700

    mess: Linux config polish plus fish cleanup, apple defaults cleanup, etc

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,22 +3,17 @@ local config = wezterm.config_builder()
 local is_mac = wezterm.target_triple:find("darwin") ~= nil
 
 config.term = "xterm-256color"
+
 config.color_scheme = "rose-pine-moon"
 
+config.window_background_opacity = 0.7
+
 if is_mac then
-	config.window_background_opacity = 0.7
 	config.window_decorations = "RESIZE"
 	config.macos_window_background_blur = 70
 else
 	config.enable_wayland = true
-	config.window_background_opacity = 0.80
 	config.window_decorations = "NONE"
-	config.window_padding = {
-		left = 12,
-		right = 12,
-		top = 8,
-		bottom = 8,
-	}
 end
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
