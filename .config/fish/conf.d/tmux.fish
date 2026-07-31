9417285523c81c4ca4cffa88a22ed811641b09b4
mxfng <m@maxfu.ng>
2026-07-31 11:27:10 -0700

    fish(tmux): confine tmux conf.d initialization to WezTerm only

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -1,6 +1,8 @@
 # Auto-attach to tmux session
+# WezTerm only
 if status is-interactive
     and not set -q TMUX
     and test -z "$VIM"
+    and test "$TERM_PROGRAM" = WezTerm
     tmux new-session -A -s base
 end
